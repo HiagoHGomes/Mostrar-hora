33575fff70543f31123ba1ad75fbd2059cd6d002
Hiago Henrique Gomes <hiago_h3@hotmail.com>
2022-06-06 23:43:14 -0300

    If else
    Adição de cumprimentos.
    Bom dia, boa tarde...

--- script/scrpit.js
+++ script/scrpit.js
@@ -12,19 +12,23 @@ function carregar( ) {
         //boa madrugada
         img.src = 'imagens/madrugada.jpg'
         document.body.style.background = '#0d0e12'
+        document.querySelector('#greetings').innerHTML="Boa madrugada!";
 
     }else if(hora>=6 && hora <12){
         //bom dia
         img.src = 'imagens/manhã.jpg'
         document.body.style.background = '#7d92b1'
+        document.querySelector('#greetings').innerHTML="Bom dia!";
     }else if (hora>=12 && hora <18){
         //boa tarde
         img.src = 'imagens/tarde.jpg'
         document.body.style.background = '#ff6002'
+        document.querySelector('#greetings').innerHTML="Boa tarde!";
     }else{
         //boa noite
         img.src = 'imagens/noite.jpg'
         document.body.style.background = '#000000'
+        document.querySelector('#greetings').innerHTML="Boa noite!";
     }
     
     let a = new Date();
